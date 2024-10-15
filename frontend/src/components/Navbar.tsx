2a2d2810b5bf74bebb4e655a63e5e93eaf518651
heppaat <pali.herrer@gmail.com>
2024-10-15 20:40:23 +0200

    css navbar progress

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,26 @@
+"use client";
 import Link from "next/link";
 import Logo from "./icons/Logo";
 import classes from "./Navbar.module.css";
+import Hamburger from "./icons/Hamburger";
+import Close from "./icons/Close";
+import { useState } from "react";
 
 export default function Navbar() {
-  return (
-    <nav className={classes.nav}>
+  const [sidebar, setSidebar] = useState<boolean>(false);
+
+  const toggleSidebar = () => {
+    setSidebar((prev) => !prev);
+  };
+
+  const navBar = sidebar ? (
+    <nav className={classes.sidebar}>
       <ul>
         <li>
-          <Link href="/">
-            <Logo />
-          </Link>
+          <Close myClass={classes.x} click={toggleSidebar} />
+        </li>
+        <li>
+          <Link href="/">Home</Link>
         </li>
         <li>
           <Link href="/about">About</Link>
@@ -22,5 +33,32 @@ export default function Navbar() {
         </li>
       </ul>
     </nav>
+  ) : (
+    <nav className={classes.nav}>
+      <ul>
+        <li>
+          <Link href="/">
+            <Logo />
+          </Link>
+        </li>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/whiskeys">Whiskeys</Link>
+        </li>
+        <li>
+          <Link href="/posts">Posts</Link>
+        </li>
+        <li onClick={toggleSidebar}>
+          <Hamburger myClass={classes.hamburger} />
+        </li>
+      </ul>
+    </nav>
   );
+
+  return <>{navBar}</>;
 }
