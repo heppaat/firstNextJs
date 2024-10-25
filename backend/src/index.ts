def795bcba23353830c0f6d1f6334ccee7b3e9ba
heppaat <pali.herrer@gmail.com>
2024-10-25 20:39:55 +0200

    backend get endpoint

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import fs from "fs/promises";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const readFile = async (filename: string) => {
+  try {
+    const rawData = await fs.readFile(
+      `${__dirname}/../backend/database/${filename}.json`,
+      "utf-8"
+    );
+    const data = JSON.parse(rawData);
+    return data;
+  } catch (error) {
+    console.error("Error reading file", error);
+    return null;
+  }
+};
+
+app.get("/posts", async (req, res) => {
+  const whiskeys = await readFile("whiskeys");
+  if (!whiskeys) return res.sendStatus(500);
+
+  res.json(whiskeys);
+});
+
+app.listen(4000);
