6becb1ed792dccd3dd2a1b264eefe8f51d423652
heppaat <pali.herrer@gmail.com>
2024-10-27 18:14:30 +0100

    whiskey component working no css

--- frontend/src/app/whiskeys/page.tsx
+++ frontend/src/app/whiskeys/page.tsx
@@ -1,19 +1,27 @@
 "use client";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Whiskey } from "@/types/types";
 import { fetchData } from "@/services/Api";
+
+const removeDuplicateNames = (arr: Whiskey[]) =>
+  arr.reduce<Whiskey[]>(
+    (acc, curr) =>
+      acc.some((whiskey) => whiskey.name === curr.name) ? acc : [...acc, curr],
+    []
+  );
 
 export default function Whiskeys() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
   const [whiskeys, setWhiskeys] = useState<Whiskey[] | null>(null);
 
+  const [selectedWhiskey, setSelectedWhiskey] = useState<string | number>("");
+
   const getWhiskeys = async () => {
     setIsLoading(true);
     try {
       const data = await fetchData("whiskeys");
       setWhiskeys(data);
-      console.log(data);
     } catch (error) {
       setError((error as Error).message || "An error occurred");
     } finally {
@@ -25,6 +33,17 @@ export default function Whiskeys() {
     getWhiskeys();
   }, []);
 
+  const uniqueWhiskeys = whiskeys ? removeDuplicateNames(whiskeys) : [];
+
+  const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const whiskeyId = parseInt(event.target.value, 10);
+    setSelectedWhiskey(whiskeyId);
+  };
+
+  const whiskeyDescriptionToRender = uniqueWhiskeys.find(
+    (whiskey) => whiskey.id === selectedWhiskey
+  );
+
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
 
@@ -34,17 +53,28 @@ export default function Whiskeys() {
         <h1>Whiskeys</h1>
       </div>
 
-      <select name="whiskeys" id="whiskey_id" defaultValue="">
+      <select
+        name="whiskeys"
+        id="whiskey_id"
+        value={selectedWhiskey}
+        onChange={handleSelect}
+      >
         <option value="" disabled>
           Choose your whiskey
         </option>
-        {whiskeys &&
-          whiskeys.map((whiskey) => (
-            <option key={whiskey.id} value={whiskey.id}>
-              {whiskey.name}
-            </option>
-          ))}
+        {uniqueWhiskeys.map((whiskey) => (
+          <option key={whiskey.id} value={whiskey.id}>
+            {whiskey.name}
+          </option>
+        ))}
       </select>
+
+      {whiskeyDescriptionToRender && (
+        <div>
+          <h1>{whiskeyDescriptionToRender.name}</h1>
+          <p>{whiskeyDescriptionToRender.description}</p>
+        </div>
+      )}
     </>
   );
 }
